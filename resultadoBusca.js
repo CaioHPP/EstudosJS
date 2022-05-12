0ee75d13a17bca5e55f2295966d9d81e0d70fcfa
CaioHPP <caioutf@gmail.com>
2022-05-12 17:24:34 -0300

    Improve no Site todo

--- resultadoBusca.js
+++ resultadoBusca.js
@@ -1,25 +1,25 @@
-const listaFilmes ={
-    page: 0,
-    results: [{
-        poster_path: "",
-        adult: false,
-        overview: "",
-        release_date: "",
-        genre_ids: [],
-        id: 0,
-        original_title: "",
-        original_language: "",
-        title: "",
-        backdrop_path: "",
-        popularity: 0,
-        vote_count: 0,
-        video: false,
-        vote_average: 0
-      }],
-    total_results: 0,
-    total_pages: 0
-}
+const listaFilmes = {
+  page: 0,
+  results: [
+    {
+      poster_path: "",
+      adult: false,
+      overview: "",
+      release_date: "",
+      genre_ids: [],
+      id: 0,
+      original_title: "",
+      original_language: "",
+      title: "",
+      backdrop_path: "",
+      popularity: 0,
+      vote_count: 0,
+      video: false,
+      vote_average: 0,
+    },
+  ],
+  total_results: 0,
+  total_pages: 0,
+};
 
-
-module.exports.listaFilmes
-
+module.exports.listaFilmes;
